c037484d7b45b93111dac4a2fb41335366817fe2
mestima <mestima@icloud.com>
2019-11-08 23:31:20 +0300

    index page update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,17 @@ function App() {
   return (
     <div className="App">
       <div className="App-main">
+        <header className="App-logo">
+          <h1>"Вжух" переводчик!</h1>
+        </header>
         <Form />
+        <a
+          className = "App-link"
+          href = "https://github.com/Mestima/vzuh_lang"
+          target = "_blank"
+        >
+          Этот проект на GitHub
+        </a>
       </div>
     </div>
   );
