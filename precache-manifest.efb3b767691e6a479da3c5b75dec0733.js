c2deb9fb8d31a7fa20034a492ac05c7413d2cca7
mestima <mestima@icloud.com>
2019-11-08 23:53:24 +0300

    Updates

---
+++ precache-manifest.efb3b767691e6a479da3c5b75dec0733.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78fe34f5c6927361e1f658404144e202",
+    "url": "/vzuh/index.html"
+  },
+  {
+    "revision": "d963de34fb5fc04c2d2e",
+    "url": "/vzuh/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "9c15e7c054aa62844559",
+    "url": "/vzuh/static/css/main.c78f2416.chunk.css"
+  },
+  {
+    "revision": "d963de34fb5fc04c2d2e",
+    "url": "/vzuh/static/js/2.5b4a4664.chunk.js"
+  },
+  {
+    "revision": "9c15e7c054aa62844559",
+    "url": "/vzuh/static/js/main.137935da.chunk.js"
+  },
+  {
+    "revision": "2ca992e483e091fa5364",
+    "url": "/vzuh/static/js/runtime-main.6ffe3d24.js"
+  }
+]);
